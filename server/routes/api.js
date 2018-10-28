ed02ba7aad72bf406d06d7a768604b78bd0d1d6e
Oleg <milkandtoastandhoneymorning@gmail.com>
2018-10-29 00:45:20 +0300

    add server and docker

---
+++ server/routes/api.js
@@ -0,0 +1,30 @@
+// Import dependencies
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+// MongoDB URL from the docker-compose file
+const dbHost = 'mongodb://database/mean-docker';
+
+// Connect to mongodb
+mongoose.connect(dbHost);
+
+// create mongoose schema
+const songSchema = new mongoose.Schema({
+  time: Number,
+  name: String
+});
+
+// create mongoose model
+const Song = mongoose.model('Song', songSchema);
+
+/* GET all users. */
+router.get('/songs', (req, res) => {
+    Song.find({}, (err, songs) => {
+		if (err) res.status(500).send(error);
+
+		res.status(200).json(songs);
+	});
+});
+
+module.exports = router;
