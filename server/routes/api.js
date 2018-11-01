782ffbe28a91b6b18ceb905ce065206f1eff2d4a
oleg shchepotkin <oleg.shchepotkin@inturn.co>
2018-11-01 16:55:19 +0300

    try to fix file upload

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,80 +5,78 @@ const router = express.Router();
 
 // MongoDB URL from the docker-compose file
 const dbHost = 'mongodb://database/mean-docker';
+let multer = require('multer');
+let GridFSStorage = require('multer-gridfs-storage');
+var Grid = require('gridfs-stream');
 
 // Connect to mongodb
-mongoose.connect(dbHost);
+const connection = mongoose.createConnection(dbHost);
+connection.once('open', function () {
 
+    let gfs = Grid(connection.db, mongoose.mongo);
 
-let multer = require('multer');
-let GridFsStorage = require('multer-gridfs-storage');
-let Grid = require('gridfs-stream');
-let db = mongoose.connection;
-let mongoDriver = mongoose.mongo;
-let gfs = new Grid(db, mongoDriver);
+    let storage = GridFSStorage({
+        db: connection,
+        gfs: gfs,
+        filename: (req, file, cb) => {
+            let date = Date.now();
+            // The way you want to store your file in database
+            cb(null, file.trackName + '-' + date + '.');
+        },
 
+        // Additional Meta-data that you want to store
+        metadata: function(req, file, cb) {
+            cb(null, { originalName: file.trackName });
+        },
 
-
-router.get('/', (req, res) => {
-    res.send('api works 3');
-});
-
-let storage = GridFsStorage({
-    gfs : gfs,
-
-    filename: (req, file, cb) => {
-        let date = Date.now();
-        // The way you want to store your file in database
-        cb(null, file.trackName + '-' + date + '.');
-    },
-
-    // Additional Meta-data that you want to store
-    metadata: function(req, file, cb) {
-        cb(null, { originalName: file.trackName });
-    },
-
-    root: 'tracks' // Root collection name
-});
+        root: 'tracks' // Root collection name
+    });
 
 // Multer configuration for single file uploads
-let upload = multer({
-    storage: storage
-}).single('file');
+    let upload = multer({
+        storage: storage
+    }).single('file');
+
+    router.get('/', (req, res) => {
+        res.send('api works 3');
+    });
 
 // Route for file upload
-app.post('/tracks', (req, res) => {
-    upload(req,res, (err) => {
-        if(err){
-            res.json({error_code:1,err_desc:err});
-            return;
-        }
-        res.json({error_code:0, error_desc: null, file_uploaded: true});
-    });
-});
-
-app.get('/tracks', (req, res) => {
-    let filesData = [];
-    let count = 0;
-    gfs.collection('tracks'); // set the collection to look up into
-
-    gfs.tracks.find({}).toArray((err, files) => {
-        // Error checking
-        if(!files || files.length === 0){
-            return res.status(404).json({
-                responseCode: 1,
-                responseMessage: "error"
-            });
-        }
-        // Loop through all the files and fetch the necessary information
-        files.forEach((file) => {
-            filesData[count++] = {
-                fileName: file.filename,
-                originalName: file.metadata.originalName,
-                contentType: file.contentType
+    router.post('/tracks', (req, res) => {
+        upload(req,res, (err) => {
+            if(err){
+                res.json({error_code:1,err_desc:err});
+                return;
             }
+            res.json({error_code:0, error_desc: null, file_uploaded: true});
         });
-        res.json(filesData);
     });
+
+    router.get('/tracks', (req, res) => {
+        let filesData = [];
+        let count = 0;
+        gfs.collection('tracks'); // set the collection to look up into
+
+        gfs.files.find({}).toArray((err, files) => {
+            // Error checking
+            if(!files || files.length === 0){
+                return res.status(404).json({
+                    responseCode: 1,
+                    responseMessage: "error"
+                });
+            }
+            // Loop through all the files and fetch the necessary information
+            files.forEach((file) => {
+                filesData[count++] = {
+                    fileName: file.filename,
+                    originalName: file.metadata.originalName,
+                    contentType: file.contentType
+                }
+            });
+            res.json(filesData);
+        });
+    });
+
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,22 +3,23 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
+var cors = require('cors');
 
 // Get our API routes
 const api = require('./routes/api');
 
 const app = express();
 
+let corsOptions = {
+    origin: 'http://localhost:4200',
+    credentials: true,
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions));
 // Parsers for POST data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// Cross Origin middleware
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  next()
-})
 
 // Set our api routes
 app.use('/', api);
