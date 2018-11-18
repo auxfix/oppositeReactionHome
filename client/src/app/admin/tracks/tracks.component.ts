6c7b8a859e2eac6eb274b947d039246e279ac081
Oleg <milkandtoastandhoneymorning@gmail.com>
2018-11-18 13:35:26 +0300

    fix upload button animation

--- client/src/app/admin/tracks/tracks.component.ts
+++ client/src/app/admin/tracks/tracks.component.ts
@@ -1,10 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 import { FileUploader } from 'ng2-file-upload';
+
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-admin-tracks',
   templateUrl: './tracks.component.html',
-  styleUrls: ['./tracks.component.scss']
+  styleUrls: ['./tracks.component.scss'],
+  animations: [
+    trigger('slideButtonAnim', [
+      state('down', style({
+        width: '2.4rem',
+        height: '2.4rem',
+        top: '0.6rem',
+        left: '0.29rem',
+        paddingTop: '0.7rem',
+        fontSize: '1.6rem',
+      })),
+      state('up', style({
+        width: '3rem',
+        height: '3rem',
+        top: 0,
+        left: 0,
+        paddingTop: '1.7rem',
+        fontSize: '2rem',
+      })),
+      transition('down => up', [
+        animate('0.2s')
+      ]),
+      transition('up => down', [
+        animate('0.2s')
+      ]),
+    ]),
+  ]
 })
 export class AdminTracksComponent implements OnInit {
 
@@ -14,10 +48,12 @@ export class AdminTracksComponent implements OnInit {
   private uploader: FileUploader;
   private trackName: String;
   private isActiveUpload: boolean;
+  private slideButtonAnimTrigger: string;
 
   ngOnInit() {
     this.uploader = new FileUploader({url: this.url, queueLimit: 1});
     this.isActiveUpload = false;
+    this.slideButtonAnimTrigger = 'down';
 
     this.uploader.onBuildItemForm = (fileItem, form) => {
       form.append('trackName', this.trackName);
@@ -25,4 +61,12 @@ export class AdminTracksComponent implements OnInit {
     };
   }
 
+  uploadTrack() {
+    this.slideButtonAnimTrigger = 'up';
+    this.uploader.uploadAll();
+  }
+
+  slideEnd() {
+    this.slideButtonAnimTrigger = 'down';
+  }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FileSelectDirective } from 'ng2-file-upload';
@@ -11,7 +12,6 @@ import { TracksComponent } from './tracks/tracks.component';
 import { AlbumComponent } from './album/album.component';
 import { AdminComponent } from './admin/admin.component';
 import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTabsModule } from '@angular/material/tabs';
 import { AdminPhotoComponent } from './admin/photo/photo.component';
 import { AdminTracksComponent } from './admin/tracks/tracks.component';
@@ -32,10 +32,10 @@ import { MatIconModule } from '@angular/material/icon';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    BrowserAnimationsModule,
     MatTabsModule,
     MatIconModule
   ],
