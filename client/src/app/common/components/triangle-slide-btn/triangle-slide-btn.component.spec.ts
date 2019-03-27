5bd9883f35fd73eb310c2282c18188d57a07c554
Oleg <milkandtoastandhoneymorning@gmail.com>
2019-03-27 08:37:29 +0300

    feat(TriangleButton): add tests

--- client/src/app/common/components/triangle-slide-btn/triangle-slide-btn.component.spec.ts
+++ client/src/app/common/components/triangle-slide-btn/triangle-slide-btn.component.spec.ts
@@ -1,28 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
-import { RoundButtonComponent } from './round-button.component';
+import { TriangleSlideBtnComponent } from './triangle-slide-btn.component';
 import { MatIconModule } from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
-describe('RoundButtonComponent', () => {
-  let fixture: ComponentFixture<RoundButtonComponent>;
-  let component: RoundButtonComponent;
+describe('Triangle slide button', () => {
+  let fixture: ComponentFixture<TriangleSlideBtnComponent>;
+  let component: TriangleSlideBtnComponent;
   let el: HTMLElement;
   let de: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RoundButtonComponent ],
+      declarations: [ TriangleSlideBtnComponent ],
       imports: [
         MatIconModule,
+        NoopAnimationsModule,
       ],
     })
       .compileComponents().then(() => {
-      fixture = TestBed.createComponent(RoundButtonComponent);
+      fixture = TestBed.createComponent(TriangleSlideBtnComponent);
       component = fixture.componentInstance;
-      component.icon = 'clear';
-      component.customStyles = {};
-      spyOn(component.clickButton, 'emit');
+      spyOn(component.clickBtn, 'emit');
       fixture.detectChanges();
       de = fixture.debugElement.query(By.css('button'));
       el = de.nativeElement;
@@ -35,6 +35,6 @@ describe('RoundButtonComponent', () => {
   it('should handle onClick properly', () => {
     el.dispatchEvent(new Event('click'));
 
-    expect(component.clickButton.emit).toHaveBeenCalled();
+    expect(component.clickBtn.emit).toHaveBeenCalled();
   });
 });
