1e218c0616e62e589437f6a4bee557762295b727
Oleg <milkandtoastandhoneymorning@gmail.com>
2019-03-28 08:31:04 +0300

    feat(file-input): add tests

---
+++ client/src/app/common/components/form/file-input/file-input.component.spec.ts
@@ -0,0 +1,36 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { FileFormComponent } from './file-input.component';
+import { MatIconModule } from '@angular/material';
+
+describe('Triangle slide button', () => {
+  let fixture: ComponentFixture<FileFormComponent>;
+  let component: FileFormComponent;
+  let el: HTMLElement;
+  let de: DebugElement;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ FileFormComponent ],
+      imports: [MatIconModule],
+    })
+      .compileComponents().then(() => {
+      fixture = TestBed.createComponent(FileFormComponent);
+      component = fixture.componentInstance;
+      spyOn(component, 'addFile');
+      fixture.detectChanges();
+      de = fixture.debugElement.query(By.css('input'));
+      el = de.nativeElement;
+    });
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+  it('should handle onClick properly', () => {
+    el.dispatchEvent(new Event('change'));
+
+    expect(component.addFile).toHaveBeenCalled();
+  });
+});
