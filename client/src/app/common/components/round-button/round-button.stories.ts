3c1857057735e50f678f149095e042918c7d5c2f
Oleg <milkandtoastandhoneymorning@gmail.com>
2019-04-04 09:58:07 +0300

    feat(Storybook): init commit

---
+++ client/src/app/common/components/round-button/round-button.stories.ts
@@ -0,0 +1,27 @@
+import { storiesOf, moduleMetadata  } from '@storybook/angular';
+import { MatIconModule } from '@angular/material/icon';
+import { RoundButtonComponent } from './round-button.component';
+
+storiesOf('round button', module)
+  .addDecorator(moduleMetadata({
+    imports: [
+      MatIconModule,
+    ],
+    declarations: [
+      RoundButtonComponent,
+    ],
+  }))
+  .add('default', () => ({
+    component: RoundButtonComponent,
+    props: {
+      icon: 'clear'
+    },
+  }))
+  .add('custom styles', () => ({
+    component: RoundButtonComponent,
+    props: {
+      icon: 'clear',
+      customStyles: {width: '5rem', height: '5rem'}
+    },
+  }));
+
