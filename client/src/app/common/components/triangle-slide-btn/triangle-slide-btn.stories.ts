3411aadb4290346eff4537c264bb591db01accff
Oleg <milkandtoastandhoneymorning@gmail.com>
2019-04-10 08:57:04 +0300

    feat(triangle button): add story for triangle button

---
+++ client/src/app/common/components/triangle-slide-btn/triangle-slide-btn.stories.ts
@@ -0,0 +1,21 @@
+import { storiesOf, moduleMetadata  } from '@storybook/angular';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TriangleSlideBtnComponent } from './triangle-slide-btn.component';
+
+import '!style-loader!css-loader!./story-styles.css';
+
+storiesOf('triangle button', module)
+  .addDecorator(moduleMetadata({
+    imports: [
+      MatIconModule,
+      BrowserAnimationsModule,
+    ],
+    declarations: [
+      TriangleSlideBtnComponent,
+    ],
+  }))
+  .add('default', () => ({
+    component: TriangleSlideBtnComponent,
+  }));
+
