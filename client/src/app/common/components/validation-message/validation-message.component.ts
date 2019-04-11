295453768da49ab71c9a1d1c9c32eef1def31694
Oleg <milkandtoastandhoneymorning@gmail.com>
2019-04-11 09:46:15 +0300

    test precommit

--- client/src/app/common/components/validation-message/validation-message.component.ts
+++ client/src/app/common/components/validation-message/validation-message.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-validation-message',
+  selector: 'validation-message',
   templateUrl: './validation-message.component.html',
   styleUrls: ['./validation-message.component.scss']
 })
