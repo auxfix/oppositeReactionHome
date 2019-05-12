d83e80e5031bdd5df2fd2f27292a5dd5c7729a21
Oleg <milkandtoastandhoneymorning@gmail.com>
2019-05-13 00:45:19 +0300

    feat(api): add new methods

--- server/src/Schemas/track.ts
+++ server/src/Schemas/track.ts
@@ -4,6 +4,7 @@ const TrackSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     logoId:  mongoose.Schema.Types.ObjectId,
     songId:  mongoose.Schema.Types.ObjectId,
+    order: Number,
     bandName: String,
     trackName: String,
 });
--- server/src/routes/tracks.ts
+++ server/src/routes/tracks.ts
@@ -38,7 +38,15 @@ db.once("open", () => {
     });
 
     // Route for file upload
-    router.post("/tracks", (req, res, next) => {
+    router.post("/tracks", async (req, res, next) => {
+        const AllTracksModel = db.model("tracks", TrackSchema);
+
+        const allTracks: any = await AllTracksModel.find();
+        let maxOrder = 0;
+        if (!!allTracks) {
+           maxOrder = Math.max(allTracks.map((track: any) => track.order));
+        }
+
         upload(req, res, (err) => {
             if (err) {
                 res.json({error_code: 1, err_desc: err});
@@ -50,6 +58,7 @@ db.once("open", () => {
             const TrackInstance = new TrackModel({
                 _id: new mongoose.Types.ObjectId(),
                 logoId: null,
+                order: maxOrder + 1,
                 songId: res.req.file.id,
                 bandName: res.req.file.metadata.bandName,
                 trackName: res.req.file.metadata.trackName, }
@@ -93,6 +102,30 @@ db.once("open", () => {
                         res.send(track);
                     }
                 });
+            }
+        });
+    });
+
+    router.get("/tracks/shift/:order/:way", (req, res, next) => {
+        const TrackModel = db.model("tracks", TrackSchema);
+
+        const allTracks = TrackModel.find({});
+        const sortedTracks: any = allTracks.sort((a: { order: number; }, b: { order: number; }) => a.order - b.order);
+        if (req.params.way === "up") {
+            if (req.params.order === 0) { next(); }
+            sortedTracks[req.params.order].order = req.params.order + 1;
+            sortedTracks[req.params.order - 1].order = req.params.order;
+        } else  if (req.params.way === "down") {
+            if (req.params.order === sortedTracks.length - 1 ) { next(); }
+            sortedTracks[req.params.order].order = req.params.order - 1;
+            sortedTracks[req.params.order + 1].order = req.params.order;
+        }
+
+        sortedTracks.save((error: any) => {
+            if (error) {
+                return next(new Error("Could not load save tracks"));
+            } else {
+                res.send(sortedTracks);
             }
         });
     });
